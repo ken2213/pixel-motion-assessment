f2c2312c4bc143095d55f878fa83a3ef9452b8c5
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-07 00:06:22 +0800

    CREATED: navigationBarTitles array variable

---
+++ src/constants/index.ts
@@ -0,0 +1,50 @@
+
+export const navigationBarTitles = [
+    {
+        title: 'home',
+        route: 'home'
+    },
+    {
+        title: 'inventory',
+        route: 'inventory'
+    },
+    {
+        title: 'specials',
+        route: 'specials',
+    },
+    {
+        title: 'finance',
+        route: 'finance',
+    },
+    {
+        title: 'service',
+        route: 'service',
+    },
+    {
+        title: 'parts',
+        route: 'parts',
+    },
+    {
+        title: 'collision center',
+        route: 'collision_center',
+    },
+    {
+        title: 'dealership',
+        route: 'dealership',
+    },
+    {
+        title: 'contact us',
+        route: 'contact_us',
+    },
+    {
+        title: 'map',
+        route: 'map',
+    },
+]
+
+
+
+
+
+
+
