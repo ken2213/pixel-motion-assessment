5d8b022781606d0f3c11834f04135cd04c45eda9
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-12 20:50:22 +0800

    TWEAKED: content, background color, and added button link to /inventory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,39 @@ import Image from "next/image";
 import React from 'react'
 import { motion } from 'framer-motion';
 import { textVariant } from '@/components/animation/motion';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div 
-      className="bg-red-900 h-full flex justify-center items-center"
+      className="bg-[#017bc1] h-full flex justify-center"
     >
-      <motion.h1 
-        className="text-[2rem] font-semibold text-red-200"
-        variants={textVariant(0.3)}
-        initial="hidden"
-        animate="show"
-      >
-        Honda Home Page
-      </motion.h1>
+      <div className="w-full lg:w-[70%] mt-4 pt-32 px-8">
+        <motion.p 
+            className="text-[0.9rem] md:text-[1rem] text-[#333333] text-center lg:text-start"
+            variants={textVariant(0.3)}
+            initial="hidden"
+            animate="show"
+          >
+            Let our family help you find the perfect vehicle for your family
+        </motion.p>
+
+        <motion.div 
+          className="flex justify-center lg:justify-start"
+          variants={textVariant(0.5)}
+          initial="hidden"
+          animate="show"
+        >
+          <Link href="/inventory">
+            <button
+              className="text-[0.9rem] md:text-[1rem] text-white font-semibold mt-8 bg-[#017bc1] hover:bg-white hover:text-[#017bc1] transition-all px-8 py-2 rounded-sm "
+            >
+              Search New Inventory
+            </button>
+          </Link>
+        </motion.div>
+
+      </div>
     </div>
   );
 }
